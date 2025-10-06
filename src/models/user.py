3d2008c1920dcd3a9b5ac546e56ed6ef087d474a
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-10-06 16:39:06 +0600

    defining user model

---
+++ src/models/user.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from typing import Optional
+from sqlmodel import SQLModel, Field
+import sqlalchemy.dialects.postgresql as pg
+from sqlalchemy import Column, String
+from sqlalchemy.sql import func
+import uuid
+
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+
+    id: uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID,
+            nullable=False,
+            primary_key=True,
+            default=uuid.uuid4
+        )
+    )
+    email: str
+    name: str
+    password_hash: str = Field(exclude=True)
+    refresh_token: Optional[str] = Field(
+        sa_column=Column(String, nullable=True)
+    )
+
+    created_at: datetime = Field(
+        sa_column=Column(pg.TIMESTAMP(timezone=True),
+                         server_default=func.now())
+    )
+    updated_at: datetime = Field(
+        sa_column=Column(
+            pg.TIMESTAMP(timezone=True),
+            server_default=func.now(),
+            onupdate=func.now()
+        )
+    )
+
+    def __repr__(self) -> str:
+        return f"<User {self.email}>"
