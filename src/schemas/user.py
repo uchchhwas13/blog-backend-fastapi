475975dabad06d60542257af9cc5137d7d0b4563
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-10-06 19:45:16 +0600

    Adding usermodel

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -1,8 +1,22 @@
 from pydantic import BaseModel, Field, EmailStr
+from datetime import datetime
+import uuid
 
 
 class UserCreateModel(BaseModel):
     name: str
     email: EmailStr
     password: str = Field(min_length=6)
-    profile_image: str | None = None
+    profile_image: str = Field(default="/images/default.png")
+    role: str = "user"
+
+
+class UserModel(BaseModel):
+    id: uuid.UUID
+    email: str
+    name: str
+    role: str
+    created_at: datetime
+    updated_at: datetime
+
+    model_config = {"from_attributes": True}
