102ff7ddf068789e2c88ad06165fed17c38a6de4
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-10-07 13:03:51 +0600

    defining route for blog creation

--- src/routes/blog_routes.py
+++ src/routes/blog_routes.py
@@ -1,3 +1,33 @@
-from fastapi import APIRouter
+from src.schemas.blog import AddBlogPostPayload
+from src.services.blog_service import BlogService
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, File, Form, status
+from sqlmodel.ext.asyncio.session import AsyncSession
+from src.db.main import get_session
+from src.dependencies import get_current_user_from_token
+from src.models.user import User
+from src.schemas.api_response import APIResponse
+from src.schemas.blog import AddBlogPostPayload, BlogModel
+from typing import Annotated
 
 blog_router = APIRouter()
+blog_service = BlogService()
+
+
+@blog_router.post("/", response_model=APIResponse[BlogModel], status_code=status.HTTP_201_CREATED)
+async def handle_add_blog_post(
+    title: str = Form(...),
+    body: str = Form(...),
+    cover_image: UploadFile = File(...),
+    current_user: User = Depends(get_current_user_from_token),
+    session: AsyncSession = Depends(get_session),
+):
+    if not current_user:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    if not cover_image:
+        raise HTTPException(status_code=400, detail="File upload failed")
+
+    blog_data = AddBlogPostPayload(title=title, body=body)
+    data = await blog_service.add_blog_post(blog_data, current_user, cover_image, session)
+
+    return APIResponse(data=data, success=True, message="Blog post created successfully")
---
+++ src/services/blog_service.py
@@ -0,0 +1,36 @@
+from sqlmodel.ext.asyncio.session import AsyncSession
+from fastapi import UploadFile
+from src.models.blog import Blog
+from src.models.user import User
+from src.schemas.blog import AddBlogPostPayload, BlogModel, BlogCreatedBy
+
+
+def build_file_url(path: str) -> str:
+    base_url = "http://localhost:8000"
+    return f"{base_url}{path}"
+
+
+class BlogService:
+    async def add_blog_post(self,
+                            payload: AddBlogPostPayload,
+                            user: User,
+                            file: UploadFile,
+                            session: AsyncSession
+                            ) -> BlogModel:
+        file_path = f"/uploads/{file.filename}"
+
+        new_blog = Blog(**payload.model_dump(),
+                        cover_image_url=file_path,
+                        created_by=user.id)
+
+        session.add(new_blog)
+        await session.commit()
+
+        return BlogModel(
+            id=str(new_blog.id),
+            title=new_blog.title,
+            body=new_blog.body,
+            cover_image_url=build_file_url(new_blog.cover_image_url),
+            created_by=BlogCreatedBy(id=str(user.id), name=user.name),
+            created_at=new_blog.created_at,
+        )
