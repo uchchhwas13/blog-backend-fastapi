e26b185e5e2a874d57c390e8d69add33400a9c81
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-10-07 13:03:31 +0600

    Defining models for blog

---
+++ src/schemas/blog.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class AddBlogPostPayload(BaseModel):
+    title: str
+    body: str
+
+
+class BlogCreatedBy(BaseModel):
+    id: str
+    name: str
+
+
+class BlogModel(BaseModel):
+    id: str
+    title: str
+    body: str
+    cover_image_url: str
+    created_by: BlogCreatedBy
+    created_at: datetime
