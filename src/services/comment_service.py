91addf2355ff8d9f3caf5241f3503f814a359ad5
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-10-10 09:43:26 +0600

    Inegrating custom error in comment APIs

--- src/services/comment_service.py
+++ src/services/comment_service.py
@@ -1,11 +1,11 @@
 from uuid import UUID
-from fastapi import HTTPException
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlmodel import select, insert
 from src.models.comment import Comment as CommentModel
 from src.models.user import User
 from src.schemas.blog import UserInfo, Comment, CommentResponse
 from src.utils import build_file_url
+from src.exceptions import ResourceNotFoundError, DatabaseError, AuthorizationError
 
 
 class CommentService:
@@ -15,16 +15,21 @@ class CommentService:
                           user: User,
                           session: AsyncSession,
                           ) -> CommentResponse:
+        try:
+            stmt = insert(CommentModel).values(
+                content=content,
+                blog_id=blog_id,
+                created_by=user.id
+            ).returning(CommentModel)
+            result = await session.exec(stmt)
+            await session.commit()
 
-        stmt = insert(CommentModel).values(
-            content=content,
-            blog_id=blog_id,
-            created_by=user.id
-        ).returning(CommentModel)
-        result = await session.exec(stmt)
-        await session.commit()
-        response = self._to_comment_response(result.scalar_one(), user)
-        return response
+            comment = result.scalar_one()
+            response = self._to_comment_response(comment, user)
+            return response
+        except Exception:
+            await session.rollback()
+            raise DatabaseError("Failed to add comment")
 
     async def update_comment(
         self,
@@ -39,12 +44,11 @@ class CommentService:
         comment = result.first()
 
         if comment is None:
-            raise HTTPException(status_code=404, detail="Comment not found")
+            raise ResourceNotFoundError("Comment", comment_id)
 
         if comment.author.id != user.id:
-            raise HTTPException(
-                status_code=403, detail="You are not allowed to edit this comment")
-
+            raise AuthorizationError(
+                "You are not allowed to edit this comment")
         comment.content = new_content
         await session.commit()
         await session.refresh(comment)
