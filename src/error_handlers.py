0b8e36b7916f3b059c8884c209027e9c23d843ba
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-10-20 12:17:20 +0600

    Updating error name

--- src/error_handlers.py
+++ src/error_handlers.py
@@ -45,7 +45,7 @@ async def validation_exception_handler(
         })
 
     return JSONResponse(
-        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
         content={
             "success": False,
             "message": "Validation error",
